74c71331ed311bc9b89eaa0261e204b08b080048
Michael Yalon <107754568+themickymix@users.noreply.github.com>
2025-02-25 20:46:56 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",  
+        destination: "https://note-be-ql9a.onrender.com/api/:path*", 
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
